9d36c28fef823d8d5e2f9dc2f53b1055ae0ee207
kmk0727 <kmg40359961@gmail.com>
2025-10-29 13:40:41 +0000

    sum

--- main.py
+++ main.py
@@ -1,13 +1,19 @@
 from typing import List
 # Skeleton code for even_list
 def even_list(int_list: List[int]) -> List[int]:
-  
-  pass
+  return_list = []
+  for i in range(len(int_list)):
+    if int_list[i] % 2 == 0:
+      return_list.append(int_list[i])
+
+  return return_list
 
 # Skeleton code for sum_of_squares_of_even
 def sum_of_squares_of_even(even_int_list: List[int]) -> int:
-  
-  pass
+  s = 0
+  for i in even_int_list:
+    s += i*i
+  return s
 
 # Main function
 def main():
